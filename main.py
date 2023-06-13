922548b979940828df4f5ff9fc9c4befbed2be42
Matt Pusch <mpusch88@gmail.com>
2023-06-13 13:57:39 -0700

    first commit. added chatgpt idea

---
+++ main.py
@@ -0,0 +1,19 @@
+# WIP
+
+# from mutagen import File
+
+# def is_drm_protected(file_path):
+#     try:
+#         audio = File(file_path)
+#         if 'DRM' in audio or 'WM/WMRDRM' in audio or 'WM/WMADRCPeakReference' in audio:
+#             return True
+#         else:
+#             return False
+#     except Exception as e:
+#         print(f"Error: {e}")
+#         return False
+
+# # Usage example
+# file_path = 'path_to_your_file.mp3'
+# is_protected = is_drm_protected(file_path)
+# print(f"DRM protected: {is_protected}")
